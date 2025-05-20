89323ffb02f3c232f556f0aca8060ba07aba98ad
aymhce <aymhce@gmail.com>
2025-05-20 23:59:13 +0200

    missing restctrl

--- src/main/java/com/aymhce/demo/api/DemoController.java
+++ src/main/java/com/aymhce/demo/api/DemoController.java
@@ -1,9 +1,11 @@
 package com.aymhce.demo.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DemoController {
-    
+
     // This is a simple REST controller that handles HTTP GET requests
     @GetMapping("/")
     public String hello() {
