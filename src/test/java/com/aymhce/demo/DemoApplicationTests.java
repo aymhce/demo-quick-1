e458b6687cb34dd3c0a34e8433d58b024084a01e
aymhce <aymhce@gmail.com>
2025-05-20 22:46:32 +0200

    first commit

---
+++ src/test/java/com/aymhce/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aymhce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
