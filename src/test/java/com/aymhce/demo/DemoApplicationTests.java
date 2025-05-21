aec31ece8b8c641b0e0aab5bfd8847c4e823b8e1
aymhce <aymhce@gmail.com>
2025-05-21 21:53:39 +0200

    add h2 profile for tests

--- src/test/java/com/aymhce/demo/DemoApplicationTests.java
+++ src/test/java/com/aymhce/demo/DemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.aymhce.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test", "h2"})
 class DemoApplicationTests {
 
 	@Test
