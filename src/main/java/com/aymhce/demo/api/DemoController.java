abb99df4ab1ee74220ece7850bca716e2d67f9d4
14463a <aymeric.hays@maif.fr>
2025-06-25 10:48:45 +0200

    update request mapping and modify greeting message in DemoController

--- src/main/java/com/aymhce/demo/api/DemoController.java
+++ src/main/java/com/aymhce/demo/api/DemoController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/")
 public class DemoController {
 
     // This is a simple REST controller that handles HTTP GET requests
     @GetMapping("/")
     public String hello() {
-         return "Hello, MAIF VIE World!";
+         return "Page accueil : hello, MAIF VIE World!";
     }
     
+    @GetMapping("/api/hello2")
+    public String hello2() {
+         return "Hello, MAIF VIE World!";
+    }
 }
